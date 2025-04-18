e54de8d1c1ff2c7e264b188a83f6240b1b5d7785
Kento Yamamoto <key7135g@gmail.com>
2025-04-18 20:01:01 +0900

    add: eventモデルを作成

---
+++ lib/models/event.dart
@@ -0,0 +1,25 @@
+class Event {
+  final String title;
+  final DateTime startDate;
+  final DateTime endDate;
+  final bool isAllDay;
+  final String category;
+
+  Event({
+    required this.title,
+    required this.startDate,
+    required this.endDate,
+    required this.isAllDay,
+    required this.category,
+  });
+
+  factory Event.fromJson(Map<String, dynamic> json) {
+    return Event(
+      title: json['title'] as String,
+      startDate: DateTime.parse(json['startDate'] as String),
+      endDate: DateTime.parse(json['endDate'] as String),
+      isAllDay: json['isAllDay'] as bool,
+      category: json['category'] as String,
+    );
+  }
+}
