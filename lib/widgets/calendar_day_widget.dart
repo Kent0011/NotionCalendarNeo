e94e991299e8bb3c00da167818204d06778334bd
Kento Yamamoto <key7135g@gmail.com>
2025-04-21 16:06:49 +0900

    fix: デザイン変更

--- lib/widgets/calendar_day_widget.dart
+++ lib/widgets/calendar_day_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import '../models/event.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-
 class CalendarDayWidget extends StatelessWidget {
   final DateTime date;
   final bool isSelected;
@@ -20,42 +19,74 @@ class CalendarDayWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final events = this.events.where((event) => isSameDay(date, event.startDate)).toList();
+    final events =
+        this.events.where((event) => isSameDay(date, event.startDate)).toList();
 
     return Container(
       decoration: BoxDecoration(
-        border: Border(
-          top: BorderSide(color: Colors.grey),
-          bottom: BorderSide(color: Colors.grey),
-          right: BorderSide(color: Colors.grey),
-          left: BorderSide(color: Colors.grey),
-        ),
+        border: Border(top: BorderSide(color: Colors.grey)),
         color:
             isSelected
                 ? const Color.fromARGB(255, 181, 221, 255)
                 : isToday
-                    ? const Color.fromARGB(255, 255, 209, 209)
-                    : Colors.white,
+                ? const Color.fromARGB(255, 255, 209, 209)
+                : Colors.white,
       ),
       child: Align(
         alignment: Alignment.topCenter,
         child: Column(
           children: [
-              Text('${date.day}', style: TextStyle(color: Colors.black)),
-            for (var event in events)
-              if (event.color == 'blue')
-                Text(event.title, style: TextStyle(color: Colors.blue, fontSize: 10), maxLines: 1, overflow: TextOverflow.ellipsis)
-              else if (event.color == 'red')
-                Text(event.title, style: TextStyle(color: Colors.red, fontSize: 10), maxLines: 1, overflow: TextOverflow.ellipsis)
-              else if (event.color == 'green')
-                Text(event.title, style: TextStyle(color: Colors.green, fontSize: 10), maxLines: 1, overflow: TextOverflow.ellipsis)
-              else if (event.color == 'yellow')
-                Text(event.title, style: TextStyle(color: Colors.orange, fontSize: 10), maxLines: 1, overflow: TextOverflow.ellipsis)
-              else if (event.color == 'purple')
-                Text(event.title, style: TextStyle(color: Colors.purple, fontSize: 10), maxLines: 1, overflow: TextOverflow.ellipsis)
-              else
-                Text(event.title, style: TextStyle(color: Colors.black, fontSize: 10), maxLines: 1, overflow: TextOverflow.ellipsis)
+            Text('${date.day}', style: TextStyle(color: Colors.black)),
+            Padding(
+              padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+              child: Column(
+                children: [
+                  for (var event in events)
+                    if (event.color == 'blue')
+                      Text(
+                        event.title,
+                        style: TextStyle(color: Colors.blue, fontSize: 10),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      )
+                    else if (event.color == 'red')
+                      Text(
+                        event.title,
+                        style: TextStyle(color: Colors.red, fontSize: 10),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      )
+                    else if (event.color == 'green')
+                      Text(
+                        event.title,
+                        style: TextStyle(color: Colors.green, fontSize: 10),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      )
+                    else if (event.color == 'yellow')
+                      Text(
+                        event.title,
+                        style: TextStyle(color: Colors.orange, fontSize: 10),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      )
+                    else if (event.color == 'purple')
+                      Text(
+                        event.title,
+                        style: TextStyle(color: Colors.purple, fontSize: 10),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      )
+                    else
+                      Text(
+                        event.title,
+                        style: TextStyle(color: Colors.black, fontSize: 10),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
